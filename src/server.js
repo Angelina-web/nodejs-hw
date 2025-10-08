262e3f5f2929864c4d4fb9e0db95ff3f5fdc4394
your_nameAnhelina <bogdanangelina@gmail.com>
2025-10-08 11:48:35 +0300

    fixed function

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ import { errors } from 'celebrate';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-const PORT = process.env.PORT ?? 3030;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors());
